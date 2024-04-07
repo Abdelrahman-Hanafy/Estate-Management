071ece8bc41454a3403f15447b55690c604247cb
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-04-07 11:32:20 +0200

    [Feature] Add Maintanace sub tasks with progress indecators

--- models/property_maintanance.py
+++ models/property_maintanance.py
@@ -25,18 +25,28 @@ class PropertyMaintanance(models.Model):
         string='Cost', help='Total Cost of the maintenance', default=0)
     start_date = fields.Date(
         string='Start Date', help='Start date of the maintenance')
-    duration = fields.Integer(string='Duration', help='Duration in workdays')
-
-    # TODO: ADD SUBTASKS AND FOLLOW WORK ORDER PROGRESS
+    
+    duration = fields.Integer(string='Duration', help='Duration in workdays', compute='_compute_duration')
+    progress = fields.Integer(string='Progress', compute='_compute_progress')
 
     ##### Relationship Fields #####
     property_id = fields.Many2one(
         'property', string='Property', default=lambda self: self.env.context.get('property_id', None))
     
-    personnel_id = fields.Many2many(
-        'hr.employee', string='worker'
-    )
+    task_ids = fields.One2many('property.maintanance.task', 'work_order_id', string='Tasks')
 
+    #### Compute ####
+    @api.depends('task_ids')
+    def _compute_duration(self):
+        for record in self:
+            record.duration = sum(record.task_ids.mapped('duration'))
+
+    @api.depends('task_ids')
+    def _compute_progress(self):
+        for record in self:
+            done = sum(record.task_ids.filtered(lambda t: t.state == 'done').mapped('duration'))
+            record.progress = (done*100 / record.duration) if record.duration else 0
+    
     #### Actions ####
     def action_start(self):
         self.state = 'in_progress'
@@ -75,3 +85,25 @@ class PropertyMaintananceRequest(models.TransientModel):
 
         property.maintenance_state = 'requested'
         return property_maintanance
+
+class PrpertyMaintananceTask(models.Model):
+    _name = 'property.maintanance.task'
+    _description = 'Maintanance Task'
+
+    name = fields.Char(string='Name')
+    description = fields.Text(string='Description')
+    state = fields.Selection([
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ], default='pending', string='State')
+    
+    duration = fields.Integer(string='Duration', help='Duration in workdays')
+
+    # Relationship Fields
+    work_order_id = fields.Many2one(
+        'property.maintanance', string='Work Order')
+    
+    personnel_id = fields.Many2many(
+        'hr.employee', string='worker'
+    )
