9af5ad88208a6e6b082857404367c44738b58ace
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-04-08 11:29:59 +0200

    [Feature] Try to connect the survey module with the property module ps: the connection between survey object anf property still does not established

--- __manifest__.py
+++ __manifest__.py
@@ -13,6 +13,8 @@
         'security/security.xml',
         'security/ir.model.access.csv',
 
+        'views/property_actions.xml',
+
         'wizard/maintanance_request.xml',  # wizard
         'wizard/property_survey.xml',
 
